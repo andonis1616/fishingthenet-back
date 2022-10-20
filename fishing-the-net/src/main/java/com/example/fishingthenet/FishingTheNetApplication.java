d22ee1b356ffbfa5bee7fcf290c478d838297949
valentin-boghean-assist <valentin.boghean@assist.ro>
2022-10-20 23:56:20 +0300

    Login

---
+++ fishing-the-net/src/main/java/com/example/fishingthenet/FishingTheNetApplication.java
@@ -0,0 +1,13 @@
+package com.example.fishingthenet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishingTheNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishingTheNetApplication.class, args);
+	}
+
+}
---
+++ fishing-the-net/src/test/java/com/example/fishingthenet/FishingTheNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fishingthenet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishingTheNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
