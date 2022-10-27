af023b2e87bce84e698f639b0014ee6b38b282ca
valentin-boghean-assist <valentin.boghean@assist.ro>
2022-10-27 23:41:04 +0300

    Login and Register

--- fishing-the-net/src/main/java/com/example/fishingthenet/config/SecurityConfig.java
+++ fishing-the-net/src/main/java/com/example/fishingthenet/config/SecurityConfig.java
@@ -1,14 +1,21 @@
 package com.example.fishingthenet.config;
 
+import com.example.fishingthenet.authentication.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
@@ -21,15 +28,49 @@ import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
-@PropertySource("classpath:application.properties")
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
 
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.authorizeRequests()
-                    .anyRequest().authenticated()
-                    .and()
-                    .oauth2Login();
-        }
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/**", "/login").permitAll()
+                .antMatchers("/api/auth/**").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    //    @Override
+//    @Bean
+//    protected UserDetailsService userDetailsService() {
+//        UserDetails ramesh = User.builder().username("ramesh").password(passwordEncoder()
+//                .encode("password")).roles("USER").build();
+//        UserDetails admin = User.builder().username("admin").password(passwordEncoder()
+//                .encode("admin")).roles("ADMIN").build();
+//        return new InMemoryUserDetailsManager(ramesh, admin);
+//    }
+}
---
+++ fishing-the-net/src/main/resources/db/migration/V1.1.1__init_data.sql
@@ -0,0 +1,31 @@
+CREATE TABLE roles
+(
+    id           BIGINT generated always as identity,
+    name         text    NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE (id)
+);
+
+
+INSERT INTO roles(name) VALUES('ROLE_ADMIN');
+
+CREATE TABLE users
+(
+    id           BIGINT generated always as identity,
+    name         text    NOT NULL,
+    username         text    NOT NULL,
+    email         text    NOT NULL,
+    password         text    NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE (id)
+);
+
+CREATE TABLE user_roles
+(
+    id          integer generated always as identity,
+    user_id     BIGINT,
+    role_id     BIGINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES roles (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+);
